92bc1f3417c3785ed3259e19ce053d2fdea1d07c
songge186 <943642543@qq.com>
2019-03-27 14:24:55 +0800

    activetylive

---
+++ app/src/main/java/com/example/a94364/practice_1/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.a94364.practice_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Log.i("MainActivityLife", "调用onCreate()");
+        }
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+            Log.i("MainActivityLife", "调用onStart()");
+        }
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            Log.i("MainActivityLife", "调用onResume()");
+        }
+
+        @Override
+        protected void onPause() {
+            super.onPause();
+            Log.i("MainActivityLife", "调用onPause()");
+        }
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+            Log.i("MainActivityLife", "调用OnStop()");
+        }
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            Log.i("MainActivityLife", "调用OnDestroy()");
+        }
+
+        @Override
+        protected void onRestart() {
+            super.onRestart();
+            Log.i("MainActivityLife", "调用onRestroy()");
+        }
+}
